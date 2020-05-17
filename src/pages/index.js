62411fb68bfb3c4c3b8e329340e4d9608d6744ad
kanatos <geoffreydu89@gmail.com>
2020-05-17 11:34:13 +0200

    11 - graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Bio from "../components/bio"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout page="1">
     <SEO title="Home" />
+    <h1>{data.site.siteMetadata.author}</h1>
     <Bio />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -18,5 +19,16 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+  }
+`
 
 export default IndexPage
